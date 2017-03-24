f1f595844e9b352d843b6f488ad33697e7e8925a
tim <1103137241@gm.kuas.edu.tw>
2017-03-24 14:08:51 +0800

    3/24

---
+++ WebApplication4/App_Start/Startup.Auth.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.AspNet.Identity
+Imports Microsoft.Owin
+Imports Microsoft.Owin.Security.Cookies
+Imports Owin
+
+Partial Public Class Startup
+    ' 如需設定驗證的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301864
+    Public Sub ConfigureAuth(app As IAppBuilder)
+        ' 讓應用程式使用 Cookie 儲存已登入使用者的資訊
+        app.UseCookieAuthentication(New CookieAuthenticationOptions() With {
+        .AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+        .LoginPath = New PathString("/Account/Login")})
+        ' Use a cookie to temporarily store information about a user logging in with a third party login provider
+        app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie)
+
+        ' 註銷下列各行以啟用利用協力廠商登入提供者登入
+        'app.UseMicrosoftAccountAuthentication(
+        '    clientId:="",
+        '    clientSecret:="")
+
+        'app.UseTwitterAuthentication(
+        '   consumerKey:="",
+        '   consumerSecret:="")
+
+        'app.UseFacebookAuthentication(
+        '   appId:="",
+        '   appSecret:="")
+
+        'app.UseGoogleAuthentication()
+    End Sub
+End Class
+
---
+++ WebApplication4/Models/AccountViewModels.vb
@@ -0,0 +1,56 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class ExternalLoginConfirmationViewModel
+    <Required>
+    <Display(Name:="使用者名稱")>
+    Public Property UserName As String
+End Class
+
+Public Class ManageUserViewModel
+    <Required>
+    <DataType(DataType.Password)>
+    <Display(Name:="目前密碼")>
+    Public Property OldPassword As String
+
+    <Required>
+    <StringLength(100, ErrorMessage:="{0} 的長度至少必須為 {2} 個字元。", MinimumLength:=6)>
+    <DataType(DataType.Password)>
+    <Display(Name:="新密碼")>
+    Public Property NewPassword As String
+
+    <DataType(DataType.Password)>
+    <Display(Name:="確認新密碼")>
+    <Compare("NewPassword", ErrorMessage:="新密碼與確認密碼不相符。")>
+    Public Property ConfirmPassword As String
+End Class
+
+Public Class LoginViewModel
+    <Required>
+    <Display(Name:="使用者名稱")>
+    Public Property UserName As String
+
+    <Required>
+    <DataType(DataType.Password)>
+    <Display(Name:="密碼")>
+    Public Property Password As String
+
+    <Display(Name:="記住我?")>
+    Public Property RememberMe As Boolean
+End Class
+
+Public Class RegisterViewModel
+    <Required>
+    <Display(Name:="使用者名稱")>
+    Public Property UserName As String
+
+    <Required>
+    <StringLength(100, ErrorMessage:="{0} 的長度至少必須為 {2} 個字元。", MinimumLength:=6)>
+    <DataType(DataType.Password)>
+    <Display(Name:="密碼")>
+    Public Property Password As String
+
+    <DataType(DataType.Password)>
+    <Display(Name:="確認密碼")>
+    <Compare("Password", ErrorMessage:="密碼和確認密碼不相符。")>
+    Public Property ConfirmPassword As String
+End Class
